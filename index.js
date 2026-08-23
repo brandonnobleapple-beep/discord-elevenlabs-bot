87dba7cd9deb7c4de45fa0db17c89e6a26bb418d
brandonnobleapple-beep <brandonnobleapple@gmail.com>
2026-08-23 10:26:00 -0600

    Create package.json for Discord bot project
    Added package.json for project metadata and dependencies.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits } = require("discord.js");
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildVoiceStates
-  ]
-});
-
-client.once("ready", () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.login(process.env.DISCORD_TOKEN);
+{
+  "name": "discord-elevenlabs-bot",
+  "version": "1.0.0",
+  "description": "Discord ElevenLabs voice bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@discordjs/opus": "^0.9.0",
+    "@discordjs/voice": "^0.19.0",
+    "discord.js": "^14.25.1",
+    "dotenv": "^17.2.1",
+    "ffmpeg-static": "^5.2.0"
+  }
+}
